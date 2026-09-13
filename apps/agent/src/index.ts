be5830c7d49545b7cca990b2b93bd3eaffcb73a7
Martin Jeppesen <67677982+swamp2k@users.noreply.github.com>
2026-09-13 22:37:00 +0200

    Export restore staging safety helpers

--- apps/agent/src/index.ts
+++ apps/agent/src/index.ts
@@ -19,6 +19,7 @@ export * from "./restic-maintenance-executor.js";
 export * from "./restic-repository-lock.js";
 export * from "./restic-restore-executor.js";
 export * from "./restic-restore-preview-executor.js";
+export * from "./restore-staging.js";
 export * from "./rtorrent-client.js";
 export * from "./runner.js";
 export * from "./runtime-config.js";
